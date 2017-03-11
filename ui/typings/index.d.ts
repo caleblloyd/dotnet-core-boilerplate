ea10490feee8ba4198afe0336268ecdab50ae1cf
Caleb Lloyd <caleblloyd@gmail.com>
2017-03-11 00:11:06 -0500

    update typings

--- ui/typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="custom/webpack.d.ts" />
-/// <reference path="globals/core-js/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="modules/express/index.d.ts" />
-/// <reference path="modules/node-fetch/index.d.ts" />

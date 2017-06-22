170d3ecb80f849ee9cf2c110c01ec6b25c4c7070
Caleb Lloyd <caleblloyd@gmail.com>
2017-06-22 16:21:14 -0400

    reverse backend/frontend check

--- ci/docker-up.sh
+++ ci/docker-up.sh
@@ -17,8 +17,8 @@ pid=$!
 for i in `seq 1 600`; do
 	# wait for containers to come up
 	sleep 1
-	# get a response from frontend and backend
-	curl localhost:48001 >/dev/null 2>&1 && curl localhost:48010 >/dev/null 2>&1
+	# get a response from backend and frontend
+	curl localhost:48010 >/dev/null 2>&1 && curl localhost:48001 >/dev/null 2>&1
 	# exit if successful
 	if [ $? -eq 0 ]; then
 		kill $pid
